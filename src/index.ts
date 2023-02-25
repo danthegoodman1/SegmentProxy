1524a6a032c557917a836f2e7e16e0b6c7f59422
Dan Goodman <dan@danthegoodman.com>
2023-02-25 17:27:15 -0500

    hello world with logger

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import WorkerLogger from "cfworkerslogger"
+
+export interface Env {}
+
+export default {
+	async fetch(
+		request: Request,
+		env: Env,
+		ctx: ExecutionContext
+	): Promise<Response> {
+		const logger = new WorkerLogger({
+			level: "DEBUG",
+			destinationFunction: async function(lines) {
+				console.log("I am sending lines", JSON.stringify(lines))
+			}
+		})
+		logger.debug("this is debug")
+		logger.warn("this is a warn")
+		const res = new Response("Hello World!");
+		ctx.waitUntil(logger.Drain())
+		return res
+	},
+};
