14ca330e6a3a59c841b0709149fdc3af13a5dcdc
Dan Goodman <dan@danthegoodman.com>
2023-02-27 13:29:24 -0500

    redact the auth header

--- src/index.ts
+++ src/index.ts
@@ -21,7 +21,12 @@ export default {
       levelKey: "severity",
       withMeta: {
         url: request.url,
-        headers: Object.fromEntries(request.headers.entries()),
+        headers: Object.fromEntries(Array.from(request.headers.entries()).map(([key, val]) => {
+          if (key.toLowerCase() === "authorization") {
+            val = "REDACTED"
+          }
+          return [key, val]
+        })),
         method: request.method
       },
       destinationFunction: async function (lines) {
